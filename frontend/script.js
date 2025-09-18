4bec90bd5f722f24989c487249f6c2e9d9cc33ad
O-Gui <guilherme.basilio2.0@gmail.com>
2025-09-18 14:32:30 -0300

    notas e organização

--- frontend/script.js
+++ frontend/script.js
@@ -2,11 +2,11 @@ const botaoAdicionar = document.getElementById('botaoAdicionar');
 const campoNovaTarefa = document.getElementById('novaTarefa');
 const listaDeTarefas = document.getElementById('listaDeTarefas');
 
-// Função para criar um novo item de tarefa na lista
+
 function criarNovoElementoDeTarefa(textoTarefa) {
     const itemDaLista = document.createElement('li');
 
-    // Cria o círculo de checkbox
+  
     const circuloDoCheckbox = document.createElement('div');
     circuloDoCheckbox.classList.add('checkbox-circle');
 
@@ -18,9 +18,9 @@ function criarNovoElementoDeTarefa(textoTarefa) {
     // Cria o ícone da lixeira
     const iconeLixeira = document.createElement('div');
     iconeLixeira.classList.add('trash-icon');
-    iconeLixeira.innerHTML = '&#x1F5D1;'; // Código para o ícone da lixeira
+    iconeLixeira.innerHTML = '&#x1F5D1;'; 
 
-    // Adiciona os elementos à lista
+
     itemDaLista.appendChild(circuloDoCheckbox);
     itemDaLista.appendChild(spanTarefa);
     itemDaLista.appendChild(iconeLixeira);
