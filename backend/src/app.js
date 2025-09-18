1dec1aa3d55310eff022756d0786ba267b3c74da
Larissa-Kailane <larissakailanelv@gmail.com>
2025-09-18 10:02:30 -0300

    feat: Configuraçao inicial do express para iniciar o desenvolvimento da feature de criar tarefas.

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,5 +3,11 @@ import {AppDataSource} from "./data-source.js";
 AppDataSource.initialize()
     .then(() => {
         console.log("Inicializando o banco;)")
+        const App = express();
+        App.use(express.json());
+        App.use("/api", tarefaRotas);
+        App.listen(3000, () => {
+            console.log ("Servidor servindo!")
+        });
     })
     .catch((error) => console.log("Erro ao inicializar o banco de dados :(", error))
