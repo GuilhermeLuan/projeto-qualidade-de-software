019423f519fae369f14951825576f65359cccab7
Guilherme Luan <guilhermeluan@pm.me>
2025-09-18 11:50:13 -0300

    feat: adiciona tratamento assíncrono para as rotas de tarefa

--- backend/src/routes/Tarefa.router.js
+++ backend/src/routes/Tarefa.router.js
@@ -5,22 +5,22 @@ import Router from "express"
 //criando as rotas
 const router = Router();
 
-router.post("/tarefas", (req, res) => {
+router.post("/tarefas", async (req, res) => {
     const {titulo, descricao} = req.body;
     const estaCompleta = false;
     const tarefaRepository = AppDataSource.getRepository(Tarefa);
     const tarefa = tarefaRepository.create({titulo, descricao, estaCompleta});
-    // Salvar/enviar o ID da tarefa pra poder deletar depois
-    tarefaRepository.save(tarefa);
+
+    const tarefaSalva = await tarefaRepository.save(tarefa);
     res.status(201).json(tarefa);
 
     console.log("Tarefa criada com sucesso!")
 });
 
-router.delete("/tarefas/:id", (req, res) => {
+router.delete("/tarefas/:id", async (req, res) => {
     const id = req.params.id;
     const tarefaRepository = AppDataSource.getRepository(Tarefa);
-    tarefaRepository.delete({
+    await tarefaRepository.delete({
         id: id
     })
     res.status(204).json();
@@ -29,11 +29,11 @@ router.delete("/tarefas/:id", (req, res) => {
 })
 
 //criando a rota para marcar e desmarcar a tarefa como completa
-router.patch("/tarefas/:id", (req, res) => {
+router.patch("/tarefas/:id", async (req, res) => {
     const id = req.params.id;
     const tarefaRepository = AppDataSource.getRepository(Tarefa);
     const estaCompleta = req.body.estaCompleta;
-    tarefaRepository.update(
+    await tarefaRepository.update(
         { id: id },
         { estaCompleta: estaCompleta }
     ); 
