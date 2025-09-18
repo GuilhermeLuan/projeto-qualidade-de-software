b638edec1a0856266fe993e6b0aef6c1605c45e8
LuisMedrado <luplfmc@gmail.com>
2025-09-18 11:10:54 -0300

    feat: deletar tarefas por ID

--- backend/src/routes/Tarefa.router.js
+++ backend/src/routes/Tarefa.router.js
@@ -10,8 +10,22 @@ router.post("/tarefas", (req, res) => {
     const estaCompleta = false;
     const tarefaRepository = AppDataSource.getRepository(Tarefa);
     const tarefa = tarefaRepository.create({titulo, descricao, estaCompleta});
+    // Salvar/enviar o ID da tarefa pra poder deletar depois
     tarefaRepository.save(tarefa);
     res.status(201).json(tarefa);
+
+    console.log("Tarefa criada com sucesso!")
 });
+
+router.delete("/tarefas/:id", (req, res) => {
+    const id = req.params.id;
+    const tarefaRepository = AppDataSource.getRepository(Tarefa);
+    tarefaRepository.delete({
+        id: id
+    })
+    res.status(204).json();
+    
+    console.log("Tarefa apagada com sucesso!")
+})
 
 export default router;
