44730bcbc2b6c26375412ce5b30809bdd824428e
Larissa-Kailane <larissakailanelv@gmail.com>
2025-09-20 23:01:27 -0300

    refactor: Atualizando o arquivo Tarefa.service.js com os métodos.

--- backend/src/services/Tarefa.service.js
+++ backend/src/services/Tarefa.service.js
@@ -12,4 +12,26 @@ export class TarefaService {
         // lógica para listar tarefas
         return await TarefaRepository.find();
     }
+
+    async deletarTarefa(id, res){
+        // lógica para deletar tarefa
+        await this.verificarTarefa(id, res);
+        return await TarefaRepository.delete({id});
+    }
+
+    //método para verificar se a tarefa existe
+    async verificarTarefa(id, res){
+        // lógica para verificar tarefa
+        const tarefa = await TarefaRepository.findOneBy({id})
+        if (!tarefa) {
+            return res.status(404).json({ message: "Tarefa inexistente." })
+        }
+        return tarefa;
+    }
+
+    async atualizarStatusTarefa(id, estaCompleta, res){
+        // lógica para atualizar o status da tarefa
+        await this.verificarTarefa(id, res);
+        return await TarefaRepository.update({id: id}, {estaCompleta: estaCompleta});
+    }
 }
